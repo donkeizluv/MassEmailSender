2b87bf822f5dfcc5d0a7e7f91cc1ca89d71739e8
donkeizluv <donkeizluv@gmail.com>
2017-08-14 17:00:38 +0700

    fix invoke append log

--- MassEmailSender/Forms/LogViewer.cs
+++ MassEmailSender/Forms/LogViewer.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MassEmailSender.Forms
@@ -17,14 +10,17 @@ namespace MassEmailSender.Forms
             InitializeComponent();
         }
         public void AppendLog(string text)
-        { 
-            Invoke((Action)delegate {
+        {
+            Visible = true;
+            if (!IsHandleCreated || !dataGridViewLog.IsHandleCreated) return;
+            if (InvokeRequired)
+                Invoke((Action) delegate { AddToGrid(text); });
+            else
                 AddToGrid(text);
-            });
         }
         private void AddToGrid(string text)
         {
-            dataGridViewLog.Rows.Add($"{DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")}", text);
+            dataGridViewLog.Rows.Add($"{DateTime.Now:dd/MM/yyyy hh:mm:ss tt}", text);
             this.Visible = true;
         }
         private void LogViewer_FormClosing(object sender, FormClosingEventArgs e)
